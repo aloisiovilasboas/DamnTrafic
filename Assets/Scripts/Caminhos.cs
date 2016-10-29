ec6e600b4186575092930e9afdbef3d4bf77556c
aloisiovilasboas <aloisiovilasboas@gmail.com>
2016-10-29 04:41:05 -0300

    fillMap change

--- Assets/Scripts/Caminhos.cs
+++ Assets/Scripts/Caminhos.cs
@@ -7,10 +7,11 @@ public class Caminhos : MonoBehaviour {
 	private Node n;
 	public float carHeight;
 	public int mapSize;
+	public int blockSize;
 
 	private int[][] mapa ;
 
-	private static int SROADX  = 1; 
+	private static int SROADX = 1; 
 	private static int SROADZ = 2;
 
 
@@ -21,15 +22,15 @@ public class Caminhos : MonoBehaviour {
 		initializeMap ();
 		//mapa = new int[][] { new int[mapSize]{}, new int[]{}};
 		fillMap ();
-		Debug.Log ("Tamanho "+mapa.GetLength(0)+" "+mapa.GetLength(1));
+	//	Debug.Log ("Tamanho "+mapa.GetLength(0)+" "+mapa.GetLength(1));
 
 	
 	}
 
 	void initializeMap(){
-		mapa =  new int[mapSize][] ;
-		for (int i = 0; i < mapSize; i++) {
-			mapa [i] = new int[mapSize];
+		mapa =  new int[(mapSize*2)+1][] ;
+		for (int i = 0; i <= mapSize*2; i++) {
+			mapa [i] = new int[(mapSize*2)+1];
 		}
 	}
 
@@ -37,24 +38,34 @@ public class Caminhos : MonoBehaviour {
 
 	void fillMap(){
 
-		foreach (Transform child in transform)
-		{
-			if (child.tag == "sroadX" )
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform child = transform.GetChild (i);
+			if (child.tag == "sroadX" || child.tag == "sroadZ")
 			{
+				int scale = 1;
 				int posX = (int)Math.Round(child.position.x);
 				int posZ = (int)Math.Round(child.position.z);
 
-				mapa[posX][posZ] = SROADX;
+				posX += 50;
+				posZ += 50;
+				scale = scale * 5;
+
+				posX = posX / scale;
+				posZ = posZ / scale;
+
+
+				if (child.tag == "sroadX") {
+					mapa [posX] [posZ] = SROADX;
+					Debug.Log ("Road x added: "+posX+ " "+posZ);
+
+				} else {
+					mapa [posX] [posZ] = SROADZ;
+					Debug.Log ("Road z added: "+posX+ " "+posZ);
+				}
+
 				
 			}
-			if (child.tag == "sroadZ" )
-			{
-				int posX = (int)Math.Round(child.position.x);
-				int posZ = (int)Math.Round(child.position.z);
 
-				mapa[posX][posZ] = SROADZ;
-
-			}
 		}
 	}
 
